02b02dec2bdc303f657f8b1ed8f52c2633339073
dariashka <dariaabaklanova@gmail.com>
2024-12-05 14:02:51 +0200

    add debrief 18

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,23 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import {resolve} from 'path'
+import {defineConfig} from 'vite'
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        payout: resolve(__dirname, 'payout/index.html'),
-        debriefs: resolve(__dirname, 'debriefs/index.html'),
-        project13: resolve(__dirname, 'debriefs/project13.html'),
-        project14: resolve(__dirname, 'debriefs/project14.html'),
-        project15: resolve(__dirname, 'debriefs/project15.html'),
-        project16: resolve(__dirname, 'debriefs/project16.html'),
-        project17: resolve(__dirname, 'debriefs/project17.html'),
-      },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                payout: resolve(__dirname, 'payout/index.html'),
+                debriefs: resolve(__dirname, 'debriefs/index.html'),
+                project13: resolve(__dirname, 'debriefs/project13.html'),
+                project14: resolve(__dirname, 'debriefs/project14.html'),
+                project15: resolve(__dirname, 'debriefs/project15.html'),
+                project16: resolve(__dirname, 'debriefs/project16.html'),
+                project17: resolve(__dirname, 'debriefs/project17.html'),
+                project18: resolve(__dirname, 'debriefs/project18.html'),
+            },
+        },
     },
-  },
-  server: {
-    historyApiFallback: true,
-  },
+    server: {
+        historyApiFallback: true,
+    },
 })
